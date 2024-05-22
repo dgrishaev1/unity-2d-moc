0cd07636ac56e679ce915f3fa7d97e5e43003d19
Daniil Grishaev <grishaev.daniil@mail.ru>
2024-05-23 01:02:54 +0300

    Feat: MoveObject rewrite

--- Assets/Scripts/MoveObject.cs
+++ Assets/Scripts/MoveObject.cs
@@ -1,42 +1,80 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MoveObject : MonoBehaviour
 {
     [SerializeField] public float movementSpeed = 1f;
     [SerializeField] private Transform moveTo;
 
+    private float movementTicks = 0;
     bool isMoving = false;
 
-    private void Update()
+    private void Movement()
     {
+        movementTicks += (Time.deltaTime * movementSpeed);
+        if (movementTicks <= 0.5f) return;
+        movementTicks = 0;
+
         var positionFrom = transform.position;
         var positionTo = moveTo.position;
-
-        if (positionTo == null) return;
 
         bool isInX = positionFrom.x == positionTo.x;
         bool isInY = positionFrom.y == positionTo.y;
 
-        if (!isInX || !isInY)
+        if (isInX && isInY)
         {
-            float step = movementSpeed * Time.deltaTime;
+            isMoving = false;
+            Debug.Log("Finish!");
+            return;
+        }
 
-            transform.position = Vector2.MoveTowards(
-                positionFrom,
-                new Vector2(
-                    (isInX || !isInY) ? positionFrom.x : positionTo.x,
-                    isInY ? positionFrom.y : positionTo.y
-                ),
-                step
-                );
+        isMoving = true;
 
-            isMoving = true;
+        // right | left
+        string directionX = (transform.position.x - positionTo.x) <= 0 ? "right" : "left";
+        // up | down 
+        string directionY = (transform.position.y - positionTo.y) <= 0 ? "up" : "down";
+
+        // Переменная для рандомного движения по осям
+        // 0 = x; 1 = y
+        float randomDirection = Random.Range(0, 2);
+        if (isInX) randomDirection = 1f;
+        if (isInY) randomDirection = 0f;
+
+        Debug.Log("Go " + (randomDirection == 1f ? "x" : "y"));
+
+        // 10% шанс отправить не туда
+        bool isGoStray = Random.Range(0, 10) == 0;
+        if (isGoStray)
+        {
+            if (randomDirection == 0f)
+            {
+                directionX = directionX == "right" ? "left" : "right";
+            }
+            else
+            {
+                directionY = directionY == "up" ? "down" : "up";
+            }
+
+            Debug.Log("Go stray");
+        }
+
+        Vector2 addVector = new Vector2(0, 0);
+
+        if (randomDirection == 0f)
+        {
+            addVector.x = directionX == "right" ? 0.25f : -0.25f;
         }
         else
         {
-            isMoving = false;
+            addVector.y = directionY == "up" ? 0.25f : -0.25f;
         }
 
+        transform.position = new Vector2(transform.position.x, transform.position.y) + addVector;
     }
 
+    private void FixedUpdate()
+    {
+
+        Movement();
+    }
 }
